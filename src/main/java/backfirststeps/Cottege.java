89dc8ba7fab2274043e83efe21f4d7e84c5b6d6b
ronaldoss8 <ronaldosssantos1@gmail.coml>
2024-12-19 18:49:50 -0300

    the two fisrt class Guest,Cottege

--- src/main/java/backfirststeps/Cottege.java
+++ src/main/java/backfirststeps/Cottege.java
@@ -1,4 +1,77 @@
 package backfirststeps;
 
+import java.util.ArrayList;
+
 public class Cottege {
+  private int code;
+  private String name;
+  private int bedAmount;
+  private ArrayList<Character> KindsOfBeds;
+  private ArrayList<Guest> guests;
+
+    public Cottege(int code, String name, int bedAmount,
+                   ArrayList<Character> kindsOfBeds, ArrayList<Guest> guests) {
+        this.code = code;
+        this.name = name;
+        this.bedAmount = bedAmount;
+        KindsOfBeds = kindsOfBeds;
+        this.guests = new ArrayList<>();
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getBedAmount() {
+        return bedAmount;
+    }
+
+    public void setBedAmount(int bedAmount) {
+        this.bedAmount = bedAmount;
+    }
+
+    public ArrayList<Character> getKindsOfBeds() {
+        return KindsOfBeds;
+    }
+
+    public void setKindsOfBeds(ArrayList<Character> kindsOfBeds) {
+        KindsOfBeds = kindsOfBeds;
+    }
+
+    public ArrayList<Guest> getGuests() {
+        return guests;
+    }
+
+    public void setGuests(ArrayList<Guest> guests) {
+        this.guests = guests;
+    }
+    public void addGuest(Guest guest){
+        guests.add(guest);
+    }
+    public void removeGuest(int cpf){
+        guests.remove(cpf);
+    }
+
+    @Override
+    public String toString() {
+        return "Cottege{" +
+                "code=" + code +
+                ", name='" + name + '\'' +
+                ", bedAmount=" + bedAmount +
+                ", KindsOfBeds=" + KindsOfBeds +
+                ", guests=" + guests +
+                '}';
+    }
 }
--- src/main/java/backfirststeps/Guest.java
+++ src/main/java/backfirststeps/Guest.java
@@ -1,4 +1,78 @@
 package backfirststeps;
 
 public class Guest {
+    private String name;
+    private int cpf;
+    private int age;
+    private Cottege cottege;
+    private String ArrivedDay;
+    private String LeftDay;
+    private String NumberOfDaysInCottege;
+
+    public Guest(String name, int cpf, int age, Cottege cottege,
+                 String arrivedDay, String numberOfDaysInCottege, String leftDay) {
+        this.name = name;
+        this.cpf = cpf;
+        this.age = age;
+        this.cottege = cottege;
+        ArrivedDay = arrivedDay;
+        NumberOfDaysInCottege = numberOfDaysInCottege;
+        LeftDay = leftDay;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Cottege getCottege() {
+        return cottege;
+    }
+
+    public void setCottege(Cottege cottege) {
+        this.cottege = cottege;
+    }
+
+    public String getArrivedDay() {
+        return ArrivedDay;
+    }
+
+    public void setArrivedDay(String arrivedDay) {
+        ArrivedDay = arrivedDay;
+    }
+
+    public String getLeftDay() {
+        return LeftDay;
+    }
+
+    public void setLeftDay(String leftDay) {
+        LeftDay = leftDay;
+    }
+
+    public String getNumberOfDaysInCottege() {
+        return NumberOfDaysInCottege;
+    }
+
+    public void setNumberOfDaysInCottege(String numberOfDaysInCottege) {
+        NumberOfDaysInCottege = numberOfDaysInCottege;
+    }
 }
